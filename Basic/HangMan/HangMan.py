adcac098835ff170d1562b5993ea8cfd040e256c
unknown <truongviet2k3@gmail.com>
2022-05-28 12:31:52 +0700

    1

---
+++ Basic/HangMan/HangMan.py
@@ -0,0 +1,15 @@
+import random
+import string # xu ly chuoi ky tu
+import time  
+from words import words
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '_' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
+str = get_valid_word(words)
+print(str)
+
+def hang_man()
---
+++ Basic/HangMan/words.py
@@ -0,0 +1,1 @@
+words = ['VIET','DEPTRAI','VUITINH']
